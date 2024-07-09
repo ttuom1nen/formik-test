b986181f5ad4539f2b0f9ca81cba59dd5adb6408
ttuom1nen <ttuominen@outlook.com>
2024-07-09 20:04:13 +0300

    Add form footer to main page

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import { Formik, Field, Form, useFormikContext } from 'formik'
+import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
 import AdditionalInfo from './AdditionalInfo'
 import Button from 'react-bootstrap/Button'
-import Input from 'react-bootstrap/Button'
-import FormFeedback from 'react-bootstrap/Button'
 import { Col, Row } from 'react-bootstrap'
 import FormField from '../../components/common/FormField'
 import FormSwitch from '../../components/common/FormSwitch'
-import FormGroup from '../../components/common/FormGroup'
+
 import Tile from '../../components/Tile'
 import { useTranslation } from 'react-i18next'
-// import Form from "react-bootstrap/Form";
-// import { Field as FormikField } from "formik";
+import ViewHeader from '../../components/ViewHeader'
+import FormFooter from '../../components/common/FormFooter'
 
 const Main = () => {
   const validatonSchema = Yup.object().shape({
@@ -45,66 +43,84 @@ const Main = () => {
       }}
     >
       {(props) => (
-        <Form>
-          <Tile title={t('basicInfo.title', { ns: 'main' })}>
-            <Row>
-              <Col>
-                <FormField
-                  id="firstName"
-                  name="firstName"
-                  placeholder="Jane"
-                  label={t('firstName', { ns: 'common' })}
-                />
-              </Col>
-              <Col>
-                <FormField
-                  id="lastName"
-                  name="lastName"
-                  placeholder="Doe"
-                  label={t('lastName', { ns: 'common' })}
-                />
-              </Col>
-            </Row>
+        <>
+          <ViewHeader text="Awaiting translation" />
+          <Form>
+            <Tile title={t('basicInfo.title', { ns: 'main' })}>
+              <Row>
+                <Col>
+                  <FormField
+                    id="firstName"
+                    name="firstName"
+                    placeholder="Jane"
+                    label={t('firstName', { ns: 'common' })}
+                  />
+                </Col>
+                <Col>
+                  <FormField
+                    id="lastName"
+                    name="lastName"
+                    placeholder="Doe"
+                    label={t('lastName', { ns: 'common' })}
+                  />
+                </Col>
+              </Row>
 
-            <FormField
-              id="email"
-              name="email"
-              placeholder="jane@acme.com"
-              type="email"
-              label={t('email', { ns: 'common' })}
-            />
+              <FormField
+                id="email"
+                name="email"
+                placeholder="jane@acme.com"
+                type="email"
+                label={t('email', { ns: 'common' })}
+              />
 
-            <FormSwitch
-              id="showAdditionalInfo"
-              name="showAdditionalInfo"
-              label={t('additionalInfo', { ns: 'common' })}
-            />
+              <FormSwitch
+                id="showAdditionalInfo"
+                name="showAdditionalInfo"
+                label={t('additionalInfo', { ns: 'common' })}
+              />
 
-            <AdditionalInfo />
-          </Tile>
-          <div
-            style={{
-              width: '100%',
-              padding: '2rem',
-              position: 'fixed',
-              bottom: 0,
-            }}
-          >
-            <Button variant="primary" type="submit">
-              {t('submit', { ns: 'common' })}
-            </Button>
-            <Button
-              variant="secondary"
-              type="submit"
-              onClick={(e) => {
-                e.preventDefault()
-                console.log(props)
+              <AdditionalInfo />
+            </Tile>
+            <div
+              style={{
+                width: '100%',
+                padding: '2rem',
+                position: 'fixed',
+                bottom: 0,
               }}
             >
-              debug
-            </Button>
-          </div>
-        </Form>
+              <Button variant="primary" type="submit">
+                {t('submit', { ns: 'common' })}
+              </Button>
+              <Button
+                variant="secondary"
+                type="submit"
+                onClick={(e) => {
+                  e.preventDefault()
+                  console.log(props)
+                }}
+              >
+                debug
+              </Button>
+            </div>
+            <FormFooter>
+              <Button variant="primary" type="submit" disabled={false}>
+                {t('submit', { ns: 'common' })}
+              </Button>
+              <Button
+                variant="secondary"
+                type="submit"
+                onClick={(e) => {
+                  e.preventDefault()
+                  console.log(props)
+                }}
+              >
+                debug
+              </Button>
+            </FormFooter>
+          </Form>
+        </>
       )}
     </Formik>
   )
