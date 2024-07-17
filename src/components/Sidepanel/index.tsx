3fdc36029dc96a70ba815444b60e0f6e00708fe7
ttuom1nen <ttuominen@outlook.com>
2024-07-17 15:53:47 +0300

    Create navigation from object

--- src/components/Sidepanel/index.tsx
+++ src/components/Sidepanel/index.tsx
@@ -2,18 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './index.module.scss'
 
+const navigation = [
+    { id: "users", title: "Users", to: "/" },
+    { id: "toggles", title: "Feature Toggles", to: "/toggles" },
+    { id: "migrations", title: "DB Migrations", to: "/migrations" }
+]
+
 const Sidepanel = () => {
     return (<div className={styles.sidepanel}>
         <ul>
-            <li>
-                <Link to="/">Users</Link>
-            </li>
-            <li>
-                <Link to="/toggles">Feature Toggles</Link>
-            </li>
-            <li>
-                <Link to="/migrations">DB Migrations</Link>
-            </li>
+            {
+                navigation.map(navItem => {
+                    return (<li key={navItem.id}>
+                        <Link to={navItem.to}>{navItem.title}</Link>
+                    </li>)
+                })
+            }
         </ul>
     </div>)
 }
