121472c8a3f71cbdc12d2866a6c51e4eadda7ccf
ttuom1nen <ttuominen@outlook.com>
2024-07-23 00:05:58 +0300

    Change Link to NavLink and add active style

--- src/components/Sidepanel/index.tsx
+++ src/components/Sidepanel/index.tsx
@@ -1,25 +1,27 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styles from './index.module.scss'
 
 const navigation = [
-    { id: "users", title: "Users", to: "/" },
+    { id: "users", title: "Users", to: "/users" },
     { id: "toggles", title: "Feature Toggles", to: "/toggles" },
     { id: "migrations", title: "DB Migrations", to: "/migrations" }
 ]
 
 const Sidepanel = () => {
-    return (<div className={styles.sidepanel}>
+    return (<nav className={styles.sidepanel}>
         <ul>
             {
                 navigation.map(navItem => {
                     return (<li key={navItem.id}>
-                        <Link to={navItem.to}>{navItem.title}</Link>
+                        <NavLink to={navItem.to} className={({ isActive, isPending }) =>
+                            isPending ? styles.isPending : isActive ? styles.active : ""
+                        }>{navItem.title}</NavLink>
                     </li>)
                 })
             }
         </ul>
-    </div>)
+    </nav>)
 }
 
 export default Sidepanel
